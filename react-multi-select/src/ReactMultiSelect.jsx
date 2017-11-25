989c7f79a8c7f1035127c66625c3e806d7a5a336
konrad.hanus <konrad.hanus@gmail.com>
2017-11-25 15:33:25 +0100

    working for only 3 level

--- react-multi-select/src/ReactMultiSelect.jsx
+++ react-multi-select/src/ReactMultiSelect.jsx
@@ -13,8 +13,10 @@ class ReactMultiSelect extends Component {
       "keyLevel3": 0,
       "optionMarka": "",
       "optionRodzaj": "",
-      "optionType": ""
-
+      "optionType": "",
+      "selectLevel1": 0,
+      "selectLevel2": 0,
+      "selectLevel3": 0
     }
   }
 
@@ -49,36 +51,55 @@ class ReactMultiSelect extends Component {
             .data[this.state.keyLevel1]
             .subcategory[this.state.keyLevel2]
             .type
-            .map((data, key) => <option value={key}>{data.name}</option>);
+            .map((data, key) => <option key={key} value={key} selected={key==="0"?"selected": null}>{data.name}</option>);
         }
 
-        self.setState({"data": data.data, "optionMarka": optionMarka, "optionRodzaj": optionRodzaj, "optionType": optionType});
+         
+        self.setState({"data": data.data, "optionMarka": optionMarka, "optionRodzaj": optionRodzaj, "optionType": optionType, "selectLevel2": 0, "selectLevel3": 0});
 
       })
   }
 
   changeLevel1(e) {
-    console.log(e.target.value);
+    //console.log(e.target.value);
 
     let optionRodzaj;
+    let optionType;
+
     if (this.state.data !== "") {
       if (this.state.data[e.target.value].subcategory !== undefined) {
-        console.log(this.state);
+        //console.log(this.state);
+        //optionRodzaj = <option value="" selected="selected">wybierz rodzaj maszyny</option>
         optionRodzaj = this
           .state
           .data[e.target.value]
           .subcategory
-          .map((data, key) => <option value={key}>{data.name}</option>);
+          .map((data, key) => <option key={key} value={key} selected={key === 0 ? "selected": null}>{data.name}</option>);
       } else {
         //przejdź do strony
       }
     }
 
-    this.setState({"keyLevel1": e.target.value, "optionRodzaj": optionRodzaj});
+    if (this.state.data !== "") {
+      if (this.state.data[e.target.value].subcategory[0].type !== undefined) {
+
+        optionType = this
+          .state
+          .data[e.target.value]
+          .subcategory[0]
+          .type
+          .map((data, key) => <option key={key} value={key} selected="selected">{data.name}</option>);
+      } else {
+        //przejdź do innej strony
+      }
+    }
+
+    this.setState({"keyLevel1": e.target.value, "optionRodzaj": optionRodzaj, "optionType": optionType, "selectLevel1": e.target.value, "selectLevel2": 0, "selectLevel3": 0});
+
   }
 
   changeLevel2(e) {
-    console.log(e.target.value);
+    //console.log(e.target.value);
     let optionType;
 
     if (this.state.data !== "") {
@@ -89,28 +110,60 @@ class ReactMultiSelect extends Component {
           .data[this.state.keyLevel1]
           .subcategory[e.target.value]
           .type
-          .map((data, key) => <option value={key}>{data.name}</option>);
+          .map((data, key) => <option key={key} value={key}>{data.name}</option>);
       } else {
         //przejdź do innej strony
       }
     }
 
-    this.setState({"keyLevel2": e.target.value, "optionType": optionType});
+    this.setState({"keyLevel2": e.target.value, "optionType": optionType, "selectLevel2": e.target.value, "selectLevel3": 0});
   }
 
   changeLevel3(e) {
     console.log(e.target.value);
+    console.log(this.state.data[this.state.selectLevel1].subcategory[this.state.selectLevel2].type[e.target.value]);
+    let id = this.state.data[this.state.selectLevel1].subcategory[this.state.selectLevel2].type[e.target.value].id;
+    let linkRewrite = this.state.data[this.state.selectLevel1].subcategory[this.state.selectLevel2].type[e.target.value].link_rewrite;
+    let link = "http://sklep.ambrozy.com.pl/"+id+"-"+linkRewrite;
+    console.log(link);
+    window.location.href = link;
+    
 
     this.setState({"keyLevel3": e.target.value});
   }
 
   render() {
 
-    console.log(this.state);
+    //console.log(this.state);
     let optionMarka = this.state.optionMarka;
     let optionRodzaj = this.state.optionRodzaj;
     let optionType = this.state.optionType;
 
+    let rodzajCounter = 0;
+    let typeCounter = 0;
+
+    let markaCounter = this.state.data.length-1;
+    if(this.state.data !== "")
+    {
+      rodzajCounter = this.state.data[this.state.selectLevel1].subcategory.length-1;
+    }
+
+    console.log(this.state.data);
+    console.log(this.state.data[this.state.keyLevel1]);
+    console.log(this.state.keyLevel1, this.state.keyLevel2);
+    if(this.state.data !== "")
+    {
+      typeCounter = this.state.data[this.state.selectLevel1].subcategory[this.state.selectLevel2].type.length-1;
+    }
+    
+ 
+    //let typeCounter = this.state.data.length;
+
+    const selectEnabled2 = <select id="test" className="form-control" onChange={this.changeLevel2.bind(this)} value={this.state.selectLevel2}>{optionRodzaj}</select>;
+    const selectDisabled2 = <select id="test2" className="form-control" ><option>Wybierz rodzaj maszyny</option></select>;
+    let select2 = this.state.selectLevel1 === 0 ? selectDisabled2: selectEnabled2;
+    
+   // console.log(this.state.selectLevel2);
     return (
       <div
         className="row"
@@ -124,7 +177,7 @@ class ReactMultiSelect extends Component {
         <div className="col-md-3 bigLetter">KUP NASZE CZĘŚCI
           <br/>Wybierz swój pojazd</div>
         <div className="col-md-3">
-          <label htmlFor="marka" className="whiteText">Marka</label><br/>
+          <label htmlFor="marka" className="whiteText">Marka ({markaCounter})</label><br/>
           <select
             className="form-control"
             onChange={this
@@ -135,18 +188,11 @@ class ReactMultiSelect extends Component {
           </select>
         </div>
         <div className="col-md-3">
-          <label htmlFor="rodzaj" className="whiteText">Rodzaj maszyny</label><br/>
-          <select
-            className="form-control"
-            onChange={this
-            .changeLevel2
-            .bind(this)}
-            value={this.state.selectLevel2}>
-            {optionRodzaj}
-          </select>
+          <label htmlFor="rodzaj" className="whiteText">Rodzaj maszyny ({rodzajCounter})</label><br/>
+          {select2}
         </div>
         <div className="col-md-3">
-          <label htmlFor="model" className="whiteText">Model/Rodzaj maszyny</label><br/>
+          <label htmlFor="model" className="whiteText">Model/Rodzaj maszyny ({typeCounter})</label><br/>
           <select
             className="form-control"
             onChange={this
